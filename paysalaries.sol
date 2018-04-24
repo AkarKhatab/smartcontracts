74d061b9dc78faa5b2e6d1d501265caacf6a8973
Akar <Akar@Akars-MacBook-Air.local>
2018-04-24 19:34:15 +0200

    Allt funkar förutom .call(). Den skickar inte ut Ether från kontraktet.

--- paysalaries.sol
+++ paysalaries.sol
@@ -3,6 +3,7 @@ contract PaySalaries {
 
     bool employeePaid;
     uint private moneyBalance;
+    uint private checkvar;
     address owner;
     mapping (address => uint) public balances;
 
@@ -10,21 +11,25 @@ contract PaySalaries {
         owner = msg.sender;
         balances[msg.sender] = msg.value;
         employeePaid = false;
+        checkvar = 0;
     }
 
     function addBalance() public payable {
         balances[msg.sender] += msg.value;
     }
-    function getSalary(uint _amount) public returns (string){
-        if (employeePaid == false && this.balance > _amount-1) {
-            balances[msg.sender] -= _amount/2;
-            if (!msg.sender.call(_amount)) {
-                return 'throw';
-                throw; // if we can't pay it forward, send it back
+    function getSalary(uint _amount) public returns (uint){
+        _amount *= 1000000000000000000;
+        checkvar += 1000;
+        if (employeePaid == false && this.balance > _amount) {
+            checkvar += 100;
+            if (msg.sender.call(_amount)) {
+                checkvar += 10;
+                 // if we can't pay it forward, send it back
             }
-            balances[msg.sender] -= _amount/2;
+            balances[msg.sender] -= _amount;
             employeePaid = true;
-            return 'not throw';
+            checkvar += 1;
+            return 2;
         }
     }
 
@@ -36,5 +41,9 @@ contract PaySalaries {
 
     function getBalance() public constant returns (uint) {
         return this.balance;
+    }
+
+    function getCheckvar() public constant returns (uint) {
+        return checkvar;
     }
 }
