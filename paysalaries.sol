9db27e9ac664d83b231c9afa24deabf0b9ed12b8
Akar <Akar@Akars-MacBook-Air.local>
2018-05-14 17:58:40 +0200

    Glöm inte att ändra addressen till Paysalaries eftersom den är hårdkodad.

--- paysalaries.sol
+++ paysalaries.sol
@@ -17,12 +17,13 @@ contract PaySalaries {
     function addBalance() public payable {
         balances[msg.sender] += msg.value;
     }
-    
+
     function getSalary(uint _amount) public {
         _amount *= 1000000000000000000;
         checkvar += 1000;
         if (employeePaid == false && this.balance > _amount) {
             checkvar += 100;
+            msg.sender.call(_amount)();
             balances[msg.sender] -= _amount;
             employeePaid = true;
             checkvar += 1;
